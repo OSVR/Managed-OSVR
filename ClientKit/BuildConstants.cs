f00c0836dd500905179db1676f7b03e4d2a80261
Ryan Pavlik <ryan@sensics.com>
2015-03-31 14:19:49 -0500

    Improve assembly info (incorporating build types into it)

---
+++ ClientKit/BuildConstants.cs
@@ -0,0 +1,61 @@
+﻿/// Managed-OSVR binding
+///
+/// <copyright>
+/// Copyright 2015 Sensics, Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+/// </copyright>
+///
+
+using System;
+using System.Collections.Generic;
+
+namespace OSVR.ClientKit
+{
+    internal class BuildConstants
+    {
+#if NET20
+        public const string FrameworkDescription = ".NET 2.0";
+#elif NET45
+        public const string FrameworkDescription = ".NET 4.5";
+#else
+#error "Building for some unrecognized .NET version!"
+#endif
+
+#if DEBUG
+        public const string Configuration = "Debug";
+#else
+        public const string Configuration = "Release";
+#endif
+
+#if MANAGED_OSVR_INTERNAL_PINVOKE
+        public const bool InternalPInvoke = true;
+        public const string InternalPInvokeString = "/Static Linking";
+#else
+        public const bool InternalPInvoke = false;
+        public const string InternalPInvokeString = "";
+#endif
+
+        public static string FullConfiguration
+        {
+            get
+            {
+                var components = new List<string>() { FrameworkDescription, Configuration };
+#if MANAGED_OSVR_INTERNAL_PINVOKE
+                components.Add("Static Linking");
+#endif
+                return String.Join("/", components.ToArray());
+            }
+        }
+    }
+}
--- ClientKit/Properties/AssemblyInfo.cs
+++ ClientKit/Properties/AssemblyInfo.cs
@@ -1,27 +1,42 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿/// Managed-OSVR binding
+///
+/// <copyright>
+/// Copyright 2015 Sensics, Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+/// </copyright>
+///
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+using OSVR.ClientKit;
+using System.Reflection;
 
-[assembly: AssemblyTitle("Managed-OSVR ClientKit")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany("Sensics, Inc.")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright("Copyright © 2014 Sensics, Inc.")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("OSVR ClientKit (" + BuildConstants.FrameworkDescription + ")")]
+[assembly: AssemblyDescription(BuildConstants.FrameworkDescription + "/" + BuildConstants.Configuration + BuildConstants.InternalPInvokeString)]
+[assembly: AssemblyConfiguration(BuildConstants.FrameworkDescription + "/" + BuildConstants.Configuration + BuildConstants.InternalPInvokeString)]
+[assembly: AssemblyCompany("OSVR/Sensics, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015 Sensics, Inc. and OSVR contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
-
