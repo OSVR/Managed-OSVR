4155b4c174db8dd2f4890e3d8f7de57a0aee42c1
JeroMiya <bell.jeremy@gmail.com>
2015-09-29 16:01:32 -0400

    Added bindings for the Locomotion interfaces, position and velocity, plus a new locomotion sample.

---
+++ ExampleClients/Locamotion/Locamotion.cs
@@ -0,0 +1,69 @@
+﻿/// Managed-OSVR binding
+///
+/// <copyright>
+/// Copyright 2014, 2015 Sensics, Inc. and contributors
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+/// </copyright>
+
+using System;
+using OSVR.ClientKit;
+
+namespace Locamotion
+{
+    class Locamotion
+    {
+        static void locamotionPosition_StateChanged(object sender, TimeValue timestamp, Int32 sensor, Vec2 report)
+        {
+            Console.WriteLine("Got report: sensor is {0}. Position is ( x: {1}, y: {2} )",
+                sensor, report.x, report.y);
+        }
+
+        static void locamotionVelocity_StateChanged(object sender, TimeValue timestamp, Int32 sensor, Vec2 report)
+        {
+            Console.WriteLine("Got report: sensor is {0}. Velocity is ( x: {1}, y: {2} )",
+                sensor, report.x, report.y);
+        }
+
+
+        static void Main(string[] args)
+        {
+            ClientContext.PreloadNativeLibraries();
+			using (OSVR.ClientKit.ClientContext context = new OSVR.ClientKit.ClientContext("com.osvr.exampleclients.managed.Locamotion"))
+            {
+                // This is just one of the paths: specifically, the Hydra's left
+                // controller's analog trigger. More are in the docs and/or listed on
+                // startup
+#if NET20
+                using (var naviPosition = NaviPositionInterface.GetInterface(context, "/me/feet/both"))
+                using (var naviVelocity = NaviVelocityInterface.GetInterface(context, "/me/feet/both"))
+#else
+                using (var naviPosition = context.GetNaviPositionInterface("/me/feet/both"))
+                using (var naviVelocity = context.GetNaviVelocityInterface("/me/feet/both"))
+#endif
+                {
+                    naviPosition.StateChanged += locamotionPosition_StateChanged;
+                    naviVelocity.StateChanged += locamotionVelocity_StateChanged;
+
+                    // Pretend that this is your application's main loop
+                    for (int i = 0; i < 1000000; ++i)
+                    {
+                        context.update();
+                    }
+
+                    Console.WriteLine("Library shut down; exiting.");
+                }
+            }
+        }
+    }
+}
